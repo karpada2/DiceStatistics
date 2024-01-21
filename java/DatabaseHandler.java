0d8c526cd08c9def43f34c1664465339ba8223d7
karpada2 <gongrossman@gmail.com>
2024-01-21 19:59:11 +0200

    Started working on some shit, deal with it

--- java/DatabaseHandler.java
+++ java/DatabaseHandler.java
@@ -2,6 +2,32 @@ import java.io.FileWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 
-public class DatabaseHandler {
+public class DatabaseHandler throws FileNotFoundException{
 
+
+    //timeToReroll can be inputted as whatever if reroll is false, won't check it at all, best idea is to default to -1
+    public static String buildRollStringID(int diceCount, int diceType, boolean reroll, int timeToReroll, boolean[] numbersToReroll, int high, int low, String divTime, int addSub) {
+        String out = diceCount + "d" + diceType;
+        if (reroll) {
+            out += "T" + timeToReroll + "re_";
+            for (int i = 1; i < numbersToReroll.length; i++) {
+                if (numbersToReroll[i]) {
+                    out += i + "_";
+                }
+            }
+        } else {
+            out += "F_";
+        }
+        out += "high" + high + "low" + low;
+        if (divTime.charAt(0) == '*' || divTime.charAt(0) == 'x' || divTime.charAt(0) == 'X') {
+            out += "time";
+        } else {
+            out += "div";
+        }
+        out += divTime.substring(1);
+        out += "add" + addSub;
+        return out;
+    }
+
+    File
 }
--- java/moreAccessible.java
+++ java/moreAccessible.java
@@ -28,30 +28,6 @@ public class moreAccessible {
         }
     }
 
-    //timeToReroll can be inputted as whatever if reroll is false, won't check it at all, best idea is to default to -1 apparently
-    public static String buildRollStringID(int diceCount, int diceType, boolean reroll, int timeToReroll, boolean[] numbersToReroll, int high, int low, String divTime, int addSub) {
-        String out = diceCount + "d" + diceType;
-        if (reroll) {
-            out += "T" + timeToReroll + "re_";
-            for (int i = 1; i < numbersToReroll.length; i++) {
-                if (numbersToReroll[i]) {
-                    out += i + "_";
-                }
-            }
-        } else {
-            out += "F_";
-        }
-        out += "high" + high + "low" + low;
-        if (divTime.charAt(0) == '*' || divTime.charAt(0) == 'x' || divTime.charAt(0) == 'X') {
-            out += "time";
-        } else {
-            out += "div";
-        }
-        out += divTime.substring(1);
-        out += "add" + addSub;
-        return out;
-    }
-
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
